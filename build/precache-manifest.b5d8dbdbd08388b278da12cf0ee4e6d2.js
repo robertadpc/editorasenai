5cbc4bae1037aa031423acbac14bd7c32a96fe7b
robertadpc <robertadpcolombo@gmail.com>
2021-09-20 20:38:36 -0300

    Primeira

---
+++ build/precache-manifest.b5d8dbdbd08388b278da12cf0ee4e6d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699ccc2c2224e5933ce8bd17514d2ff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "917b096c31e74048427d",
+    "url": "/static/css/main.618940e1.chunk.css"
+  },
+  {
+    "revision": "bb078f44b16c527ababf",
+    "url": "/static/js/2.c36ddaa9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c36ddaa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917b096c31e74048427d",
+    "url": "/static/js/main.b0049580.chunk.js"
+  },
+  {
+    "revision": "e9419607096510474d1a",
+    "url": "/static/js/runtime-main.81c04eb7.js"
+  }
+]);
